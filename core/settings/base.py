e108f7c362b72b86c848358bb397581176fa7f17
Yacer Baba <yacerbaba10@gmail.com>
2024-05-06 12:26:01 +0100

    Allow all origins CORS

--- core/settings/base.py
+++ core/settings/base.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'apps.blogs',
 ]
 
-CORS_ALLOWED_ORIGINS = ['*']
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = []
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
