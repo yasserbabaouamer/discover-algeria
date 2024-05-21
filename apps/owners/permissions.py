b5a33bdd4e5da970ace765ad710b120a25e839d7
Yacer Baba <yacerbaba10@gmail.com>
2024-05-21 23:28:37 +0100

    added user authentication in owners permission 21-05-2024

--- apps/owners/permissions.py
+++ apps/owners/permissions.py
@@ -6,7 +6,9 @@ from apps.owners.models import Owner
 class IsOwnerOrAdmin(BasePermission):
 
     def has_permission(self, request, view):
-        return request.user.has_owner_account() or request.user.is_superuser
+        if request.user.is_authenticated:
+            return request.user.has_owner_account() or request.user.is_superuser
+        return False
 
     def has_object_permission(self, request, view, owner: Owner):
         if request.user.is_superuser:
